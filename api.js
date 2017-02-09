6787db274ab5a31c8ca733de5416b4ee744b5038
zerob13 <i@zerob13.in>
2017-02-09 16:31:38 +0800

    add slack lib

---
+++ api.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zerob13 on 2/9/17.
+ */
+
+const END_POINT = 'http://open.memobird.cn/home';
+const BIND_USER = '/setuserbind';
+const PRINT_PAPER = '/printpaper';
+
+export function
+
+
