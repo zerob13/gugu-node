e1ab98e84919a68e09fb371ef663e9e1cae76282
zerob13 <i@zerob13.in>
2017-02-09 17:58:37 +0800

    add post

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ router.post('/slack', async(ctx, next) => {
   await next();
   console.dir(ctx.request.body);
   let content = ctx.request.body.user_name + ' says: ' + ctx.request.body.text.replace(ctx.request.body.trigger_word, '');
-  printPaper(config.ak, getNowTime(), content, 'T', config.deviceId, userID);
-  ctx.body = {'text': 'printed!'};
+  let result = await printPaper(config.ak, getNowTime(), content, 'T', config.deviceId, userID);
+  ctx.body = {'text': 'print result: ' + result.showapi_res_error};
 });
 
 let app = new koa();
