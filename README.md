527e23440b34de9c75209de29c046321b571abde
zerob13 <i@zerob13.in>
2017-02-09 14:59:05 +0800

    config pm2

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Gugu node
 ```
 I don't know what to do.
 ```
+
+## Usage
+
+1. copy config.js.example to config.js
+
+1. modify config.j
--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 import koa from 'koa';
+import config from './config';
+import http from 'http';
 
 let app = new koa();
+const port = process.env.NODE_PORT;
+let server = http.createServer(app.callback());
+server.listen(port, () => {
+  console.log('gugu server on ' + port);
+});
 
